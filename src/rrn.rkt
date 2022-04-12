5585a0684a9f34b9873eee2f8182888bc7e019d9
ikrz <igor.krzywda@improvoc.com>
2022-04-12 19:44:05 +0200

    querying for rows

--- src/rrn.rkt
+++ src/rrn.rkt
@@ -57,7 +57,7 @@
     (cond
       [(equal? type 'string) (lambda (x y) (string<? x y))]
       [(equal? type 'number) (lambda (x y) (< x y))]
-      [(equal? type 'boolean) (lambda (x y) (not (= x y)))])))
+      [(equal? type 'boolean) (lambda (x y) (and x y))])))
 
 (define (sorter cmp ll i)
   (sort ll (lambda (a b) (if (cmp (list-ref a i) (list-ref b i)) #t #f))))
@@ -71,6 +71,31 @@
                                    (index-of (col-names tab) (car cols))))])
         (table-sort (cdr cols) sorted))))
 
+(define-struct and-f (l r))
+(define-struct or-f (l r))
+(define-struct not-f (e))
+(define-struct eq-f (name val))
+(define-struct eq2-f (name name2))
+(define-struct lt-f (name val))
+
+(define (filter-tab-rows filter-fun header tab)
+  (let ([col-elems (flatten (table-rows (table-project (list header) tab)))])
+    (foldr (lambda (a b res) (if (filter-fun a) (cons b res) res)) '() col-elems (table-rows tab))))
+
+(define (query form tab)
+  (cond
+    [(eq-f? form) (filter-tab-rows (λ (el) (equal? el (eq-f-val form))) (eq-f-name form) tab)]
+    [(lt-f? form)
+     (filter-tab-rows (λ (el) ((comparison-predicate (lt-f-name form) tab) el (lt-f-val form)))
+                      (lt-f-name form)
+                      tab)]
+    [(and-f? form) (set-intersect (query (and-f-l form) tab) (query (and-f-r form) tab))]
+    [(or-f? form) (set-union (query (or-f-l form) tab) (query (or-f-r form) tab))]
+    [(not-f? form) (set-subtract (table-rows tab) (query (not-f-e form) tab))]))
+
+(define (table-select form tab)
+  (table (table-schema tab) (query form tab)))
+
 ;; TEST DATA - will be replaced with tests
 
 (define cities
@@ -83,9 +108,20 @@
                (list "Poznań" "Poland" 262 #f)
                (list "Berlin" "Germany" 892 #t)
                (list "Munich" "Germany" 310 #f)
-               (list "Paris" "France" 105 #t)
+               (list "Paris" "France" 100 #t)
                (list "Rennes" "France" 50 #f))))
 
 (define countries
   (table (list (column-info 'country 'string) (column-info 'population 'number))
          (list (list "Poland" 38) (list "Germany" 83) (list "France" 67) (list "Spain" 47))))
+
+(define test-query (or-f (eq-f 'country "Poland") (eq-f 'country "Germany")))
+(define test-query2 (lt-f 'area 400))
+
+; (filter-tab-rows (lambda (x) (equal? x "Paris")) 'city cities)
+
+(table-rows (table-select (and-f (eq-f 'capital #t) (not-f (lt-f 'area 300))) cities))
+
+(table-select (not-f (lt-f 'area 300)) cities)
+
+(table-select (eq-f 'capital #t) cities)
